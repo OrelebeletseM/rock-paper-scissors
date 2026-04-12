1666151ba77f6c2487bc4648256a5ca68905d692
Orelebeletse Marupi <marupiorelebeletse@gmail.com>
2026-04-12 19:52:25 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello World!");
+
+function getComputerChoice(){
+    //generate random number between 0 and 1
+    const randomNumber = Math.random();
+
+    //return choice based on random number
+    if(randomNumber < 0.33){
+        return "rock";
+
+    }else if (randomNumber < 0.66){
+        return "paper";
+
+    }else{
+        return "scissors";
+    }
+}
+
+console.log("Testing computer choices");
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
